ed6a21ffff2f40d939aea10065c3d8ebeb6c208f
Farrah Fazirah <farrahfazirah@gmail.com>
2020-04-10 02:23:11 +0800

    Changed filtering style

--- js/filter.js
+++ js/filter.js
@@ -1,23 +1,63 @@
-//Filter News
-$('select#sort-news').change(function() {
-    var filter = $(this).val()
-    filterList(filter);
+// external js: isotope.pkgd.js
+Isotope.Item.prototype._create = function() {
+  // assign id, used for original-order sorting
+  this.id = this.layout.itemGUID++;
+  // transition objects
+  this._transn = {
+    ingProperties: {},
+    clean: {},
+    onEnd: {}
+  };
+  this.sortData = {};
+};
+
+Isotope.Item.prototype.layoutPosition = function() {
+  this.emitEvent( 'layout', [ this ] );
+};
+
+Isotope.prototype.arrange = function( opts ) {
+  // set any options pass
+  this.option( opts );
+  this._getIsInstant();
+  // just filter
+  this.filteredItems = this._filter( this.items );
+  // flag for initalized
+  this._isLayoutInited = true;
+};
+
+// layout mode that does not position items
+Isotope.LayoutMode.create('none');
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.card-custom',
+  layoutMode: 'none'
 });
-
-//News filter function
-function filterList(value) {
-    var list = $(".news-list .news-item");
-    $(list).fadeOut("fast");
-    if (value == "All") {
-        $(".news-list").find("article").each(function (i) {
-            $(this).delay(200).slideDown("fast");
-        });
-    } else {
-        //Notice this *=" <- This means that if the data-category contains multiple options, it will find them
-        //Ex: data-category="Cat1, Cat2"
-        $(".news-list").find("article[data-category*=" + value + "]").each(function (i) {
-            $(this).delay(200).slideDown("fast");
-        });
-    }
-}
-
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
